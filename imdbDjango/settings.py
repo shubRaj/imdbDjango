fd25928f40b441059531fb820645bb7c62752bb0
Shub Raj Lama <shuvraj1234@gmail.com>
2020-11-27 21:08:19 +0545

    imdbDjango added

--- imdbDjango/settings.py
+++ imdbDjango/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'imdbDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get("DB_ENGINE"),
+        'NAME': os.environ.get("DB_NAME"),
+        'PORT':os.environ.get("DB_PORT"),
+        'HOST':os.environ.get("DB_HOST"),
+        "USER":os.environ.get("DB_NAME"),
+        "PASSWORD":os.environ.get("DB_PASSWORD")
     }
 }
 
