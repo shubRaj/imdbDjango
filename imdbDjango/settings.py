2c95fc3d87546112008c75ce72cc2443503e9a5f
Shub Raj Lama <shuvraj1234@gmail.com>
2020-11-27 21:17:23 +0545

    imdbDjango added

--- imdbDjango/settings.py
+++ imdbDjango/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_ROOT = BASE_DIR/"media"
+MEIDA_URL = "/media/"
 STATIC_ROOT = BASE_DIR/"staticfiles"
 STATICFILES_DIRS = [
 (BASE_DIR/"static_files"),
